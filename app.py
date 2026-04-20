e2205f191194f5fe24b0c160389231a223e1b9ed
bcu-jk <jan.krasniewicz@bcu.ac.uk>
2026-04-20 20:55:41 +0100

    Test app

--- app.py
+++ app.py
@@ -1,3 +1,38 @@
 import streamlit as st
 
-st.title("Hello")
+st.title('Data Dashboard')
+st.write('Week 9: Interactive Dashboard')
+
+import csv
+values = []
+with open('student.csv', 'r', newline='') as f:
+    reader = csv.reader(f)
+    header = next(reader)
+    for row in reader:
+        values.append(float(row[2]))
+
+if st.button('Load Data'):
+    st.write(values)
+
+if st.button('Show Chart'):
+    st.bar_chart(values)
+
+threshold = st.slider('Select threshold', 0, 100)
+count = 0
+
+for v in values:
+    if v > threshold:
+        count += 1
+
+st.write('Values above threshold:', count)
+
+if st.button('Analyse Data'):
+    total = 0
+
+    for v in values:
+       total += v
+    
+    average = total / len(values)
+    
+    st.write('Average:', average)
+    st.bar_chart(values)
